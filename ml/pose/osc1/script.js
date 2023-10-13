e52165f15b085c57b67152f6c1a6e91d4060d51e
SimonJeppsson <Simon1jeppsson@gmail.com>
2023-10-14 00:47:54 +0200

    New stuff

--- ml/pose/osc1/script.js
+++ ml/pose/osc1/script.js
@@ -1,8 +1,13 @@
 // @ts-ignore
+// @ts-ignore
 import { Remote } from "https://unpkg.com/@clinth/remote@latest/dist/index.mjs";
 import * as Dom from '../../../ixfx/dom.js';
 import { Points } from '../../../ixfx/geometry.js';
 import * as MoveNet from "../Poses.js";
+import { frequencyTimer } from '../../../ixfx/flow.js';
+import { Oscillators } from '../../../ixfx/modulation.js';
+
+
 
 const getKeypoint = MoveNet.Coco.getKeypoint;
 const box = document.getElementById('box');
@@ -14,8 +19,12 @@ circleElement.style.height = "50px";
 circleElement.style.borderRadius = "50%";
 circleElement.style.backgroundColor = "blue";
 circleElement.style.position = "absolute";
-circleElement.style.transition = "all 1s ease-in-out";
+circleElement.style.transition = "1s ease-in-out";
+circleElement.style.left = "50%";
+circleElement.style.top = "50%";
+circleElement.style.alignItems = "center";
 document.body.appendChild(circleElement);
+
 
 const circleElement1 = document.createElement("div1");
 circleElement1.style.width = "50px";
@@ -39,12 +48,16 @@ document.body.appendChild(circleElement2);
 
 
 const settings = Object.freeze({
+  osc: Oscillators.sine(frequencyTimer(0.5)),
   updateRateMs: 100,
   remote: new Remote(),
   poses: new MoveNet.PosesTracker({ maxAgeMs: 500 }),
 });
 
 let state = Object.freeze({
+  /** @type {number} */
+  oscValue: 0,
+  //
   bounds: {
     width: 0, height: 0,
     center: { x: 0, y: 0 },
@@ -73,13 +86,14 @@ let previousDistance = 0; // Stores the previous distance between two heads
 
 const update = () => {
   const { poses } = settings;
+  const { osc } = settings;
 
+  let oscVal = osc.next().value;
   const heads = [];
   for (const pose of poses.getRawPoses()) {
     const head = computeHead(pose);
     heads.push(head);
   }
-
   // Update the ball's position to be in the center between two heads
   if (heads.length >= 2) {
     const xCenter = (heads[0].x + heads[1].x) / 2;
@@ -95,17 +109,25 @@ const update = () => {
 
 
     // Calculates distance between point a and b
-    const distance = Points.distance(a, b); // Returns a number
-    const speed = Math.abs(distance - previousDistance);
+    const distance = Points.distance(a, b); // Returns a numbers
+    // calculate the speec between two heads
+    const speed = Math.abs(((distance - previousDistance) * 100) * 5);
+
     previousDistance = distance; // Update the previous distance for the next frame
 
-    console.log(speed)
+
 
     // Update the previous distance for the next frame
     previousDistance = distance;
+    const Value = ((oscVal * 100));
 
-    circleElement.style.left = `${xCenter * 100}vw`;
-    circleElement.style.top = `${yCenter * 100}vh`;
+    console.log(speed);
+    //circleElement.style.left = `${xCenter * 100}vw`;
+    //circleElement.style.top = `${yCenter * 100}vh`;
+    // make the circle element follow the oscilator
+    circleElement.style.left = `${Value}vw`;
+    //circleElement.style.transition = ".5s ease-in-out";
+
 
     circleElement1.style.left = `${a.x * 100}vw`;
     circleElement1.style.top = `${a.y * 100}vh`;
